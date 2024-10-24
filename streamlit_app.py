b7aa1cabb454283b7d1a893fb5d6b33a612798d1
Antonio-3 <134576207+Antonio-3@users.noreply.github.com>
2024-10-23 23:15:28 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -271,8 +271,8 @@ if seleccion_menu == "Jefe de grupo":
                             pdf.ln()
                         # Guardar el archivo PDF
                         
-                        pdf.cell(200, 10, 'Este informe detalla la impartición de clases de la materia '+materia[2]+ ' indicando si la materia ', ln=True)
-                        pdf.cell(200, 10, 'fue enseñada por diferentes profesores a lo largo del periodo y la asistencia correspondiente de cada uno.', ln=True)
+                        pdf.cell(200, 10, 'Este informe detalla la impartición de clases de la materia '+materia[2]+ ' indicando si la materia fue enseñada  ', ln=True)
+                        pdf.cell(200, 10, 'por diferentes profesores a lo largo del periodo y la asistencia correspondiente de cada uno.', ln=True)
                         pdf.output('Reporte_profe.pdf')
                         pdf.cell(20, 10, 'La materia: ', ln=True, align='C')
                         pdf.cell(20, 10, seleccion_materiaxd, ln=False, align='C')
